2f0e0a2b17f665f550e36bd3f4c7e3849c6bb744
Bouloute <annaick1995@gmail.com>
2020-06-03 15:02:16 -0400

    Starting the server + user creation view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :new]
+
+  get '/signup', to: 'users#new'
+  #get '/signin', to: 'sessions#new'
+  #post '/signin', to: 'sessions#create'
+  #delete '/signout', to: 'sessions#destroy'
 end
