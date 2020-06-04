de1842d7295656d092e78432cf454c65eee27866
Bouloute <annaick1995@gmail.com>
2020-06-03 22:12:33 -0400

    added validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_secure_password
 
     #validations
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true #inclusion @  
 
     #relations
