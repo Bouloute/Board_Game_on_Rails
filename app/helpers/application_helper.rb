646b7cf13ceec5863c601e0e2475a80788c50efa
Bouloute <annaick1995@gmail.com>
2020-06-07 16:07:25 -0400

    navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    
+    def display_user_or_home_page
+        #binding.pry 
+        if current_user
+            link_to current_user.name, user_path(current_user)
+        else 
+            link_to "Home", "/"
+        end
+        #<a href="/">USER NAME</a>
+    end
+
+    def display_log_in_out
+        if current_user
+            link_to "Logout", "/logout", method:'delete' 
+        else 
+            link_to "Login", "/login"
+        end
+    end
 end
