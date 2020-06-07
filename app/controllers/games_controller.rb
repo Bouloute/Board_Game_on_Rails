96f41f76e6314d356a61322871a10b13e4e7ddba
Bouloute <annaick1995@gmail.com>
2020-06-07 17:07:44 -0400

    fixed unlogical path + fixed game creation

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
 
     def create 
-        Game.create(user: params[:user_id], board_game: params[:board_game_id])
+        Game.create(user_id: params[:game][:user_id], board_game_id: params[:game][:board_game_id])
+        
         redirect_to user_path(current_user)
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         if @user.id
             return render :new unless @user.authenticate(params[:user][:password])
             session[:user_id] = @user.id
-            redirect_to users_path #board_games_path
+            redirect_to board_games_path
         else
             render :new 
         end
