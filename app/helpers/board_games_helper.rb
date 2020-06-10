9943315e3d71ee6d35bc0d8ee4666be00cc787d2
Bouloute <annaick1995@gmail.com>
2020-06-10 17:58:02 -0400

    DRYing it up

--- app/helpers/board_games_helper.rb
+++ app/helpers/board_games_helper.rb
@@ -1,8 +1,20 @@
 module BoardGamesHelper
     
     def display_user_name_if_not_current
-        if current_user && params[:user_id] && !(params[:user_id] == current_user.id.to_s)
-            "Games "+ User.find_by(id: params[:user_id]).name + " has played :"
+        if current_user && params[:user_id] 
+            if !(params[:user_id] == current_user.id.to_s)
+                "Games "+ User.find_by(id: params[:user_id]).name + " has played :"
+            else
+                "Games I have played"
+            end
+        end
+    end
+
+    def display_form_for_search_bar
+        if params[:user_id]
+            user_board_games_path(params[:user_id])
+        else 
+            board_games_path
         end
     end
 end
